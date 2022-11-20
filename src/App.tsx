65a9a026a8683fa4918d08fc0a21a44160a41e51
Timur Kakhramonov <macbookuser@users-MacBook-Pro.local>
2022-11-20 11:31:56 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import style from "./App.module.scss";
+import Test from "./components/Test";
+
+function App() {
+  return (
+    <div className={style.App}>
+       <Test/>
+    </div>
+  );
+}
+
+export default App;
